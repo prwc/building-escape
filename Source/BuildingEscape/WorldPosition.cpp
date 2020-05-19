31350c89d727fa9cc946b16c79131b42fccb2fb2
Peerawit Canthasaksiri <5309753+prwc@users.noreply.github.com>
2020-05-19 21:49:24 +0700

    world position actor component

---
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WorldPosition.h"
+#include "Gameframework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Display, TEXT("Actor Name: %s"), *GetOwner()->GetName());
+	UE_LOG(LogTemp, Display, TEXT("Actor Location: %s"), *GetOwner()->GetActorLocation().ToString());
+}
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
