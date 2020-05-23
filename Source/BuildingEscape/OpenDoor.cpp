c73fa1fd9027106b304f6de78b2df23002c96765
Peerawit Canthasaksiri <5309753+prwc@users.noreply.github.com>
2020-05-24 00:47:46 +0700

    add open door actor component

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator NewRotator = GetOwner()->GetActorRotation();
+	NewRotator.Yaw = 90.0f;
+	GetOwner()->SetActorRotation(NewRotator);
+}
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
